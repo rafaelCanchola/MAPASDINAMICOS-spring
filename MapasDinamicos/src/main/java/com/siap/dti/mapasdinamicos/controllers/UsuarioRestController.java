0b76a3fc303a8bcd52585baa9035f4508eb19b7c
Rafa <rafa_zkeleton@hotmail.com>
2021-04-07 15:55:06 -0500

    Limpieza codigo

--- MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/controllers/UsuarioRestController.java
+++ MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/controllers/UsuarioRestController.java
@@ -1,6 +1,5 @@
 package com.siap.dti.mapasdinamicos.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,22 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.siap.dti.mapasdinamicos.models.entity.Poligono;
-import com.siap.dti.mapasdinamicos.models.entity.Punto;
-import com.siap.dti.mapasdinamicos.models.entity.PuntoJson;
 import com.siap.dti.mapasdinamicos.models.entity.Usuario;
-import com.siap.dti.mapasdinamicos.models.services.IPoligonoService;
-import com.siap.dti.mapasdinamicos.models.services.IPuntoService;
 import com.siap.dti.mapasdinamicos.models.services.IUsuarioService;
-import com.vividsolutions.jts.geom.Coordinate;
-import com.vividsolutions.jts.geom.Envelope;
-import com.vividsolutions.jts.geom.Geometry;
-import com.vividsolutions.jts.geom.GeometryFactory;
-import com.vividsolutions.jts.geom.Point;
-import com.vividsolutions.jts.geom.PrecisionModel;
 
 @CrossOrigin(origins= {"http://localhost"})
 @RestController
