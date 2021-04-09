27d15ad75d0908bfe896044089c24d73f1b5813f
Rafa <rafa_zkeleton@hotmail.com>
2021-04-09 16:32:36 -0500

    Mapeo CORS de las rutas

--- MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/MapasDinamicosApplication.java
+++ MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/MapasDinamicosApplication.java
@@ -1,13 +1,38 @@
 package com.siap.dti.mapasdinamicos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MapasDinamicosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MapasDinamicosApplication.class, args);
+	}
+	
+	@Autowired
+	private Environment env;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/login").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/usuarios").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/years").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/predios").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/insertpoli").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/poliall").allowedOrigins(env.getProperty("app.cross.origins"));
+				registry.addMapping("/api/uploadcharge").allowedOrigins(env.getProperty("app.cross.origins"));
+				
+			}
+		};
 	}
 
 }
