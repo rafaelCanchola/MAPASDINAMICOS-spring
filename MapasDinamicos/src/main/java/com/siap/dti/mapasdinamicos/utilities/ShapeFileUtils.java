d651d043090fb9358b858f10b432b9c7ac8c0b6c
Rafa <rafa_zkeleton@hotmail.com>
2021-04-15 17:29:41 -0500

    Cambios en la lectura del shape

--- MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/utilities/ShapeFileUtils.java
+++ MapasDinamicos/src/main/java/com/siap/dti/mapasdinamicos/utilities/ShapeFileUtils.java
@@ -15,14 +15,16 @@ import org.geotools.data.DataStoreFinder;
 import org.geotools.data.FeatureSource;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
+import org.locationtech.jts.geom.Geometry;
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.feature.simple.SimpleFeatureType;
 
 import com.siap.dti.mapasdinamicos.models.entity.Poligono;
+import com.siap.dti.mapasdinamicos.models.entity.Shape;
 
 public class ShapeFileUtils {
-	public static List<Poligono> ReadShapeIntoPoligono(List<String> shapesNames) throws IOException{
-		List<Poligono> poligonoIterator = new ArrayList<>();
+	public static List<Shape> readShapeIntoObject(List<String> shapesNames) throws IOException{
+		List<Shape> poligonoIterator = new ArrayList<>();
         
 		for (String shapeName : shapesNames) {
         	URL shapeURL = new File(shapeName).toURI().toURL();
@@ -38,49 +40,68 @@ public class ShapeFileUtils {
             
             while (featureIterator.hasNext()) {
             	SimpleFeature feature = featureIterator.next();
-                Poligono poligono = new Poligono();
-                poligono.setObjectid((Long)feature.getAttribute("OBJECTID"));
-                poligono.setAnio((Long)feature.getAttribute("Anio"));
-                poligono.setIdestado(Long.valueOf((String)feature.getAttribute("Idestado")));
-                poligono.setNomestado(new String(((String)feature.getAttribute("Nomestado")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setIdddr((Long)feature.getAttribute("Idddr"));
-                poligono.setNomddr(new String(((String)feature.getAttribute("Nomddr")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setIdcader((Long)feature.getAttribute("Idcader"));
-                poligono.setNomcader(new String(((String)feature.getAttribute("Nomcader")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setIdmunicipio(Long.valueOf((String)feature.getAttribute("Idmunicipi")));
-                poligono.setNommunicip(new String(((String)feature.getAttribute("Nommunicip")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setConcate(Long.valueOf((String)feature.getAttribute("Concate")));
-                poligono.setIdciclo((Long)feature.getAttribute("Idciclo"));
-                poligono.setNomcliclopr((String)feature.getAttribute("Nomciclopr"));
-                poligono.setIdmodalida((Long)feature.getAttribute("Idmodalida"));
-                poligono.setNommodalid((String)feature.getAttribute("Nommodalid"));
-                poligono.setIdunidadme((Long)feature.getAttribute("Idunidadme"));
-                poligono.setNomunidad((String)feature.getAttribute("Nomunidad"));
-                poligono.setIdcultivo((Long)feature.getAttribute("Idcultivo"));
-                poligono.setNomcultivo((String)feature.getAttribute("Nomcultivo"));
-                poligono.setSembrada((Double)feature.getAttribute("Sembrada"));
-                poligono.setCosechada((Double)feature.getAttribute("Cosechada"));
-                poligono.setSiniestrad((Double)feature.getAttribute("Siniestrad"));
-                poligono.setVolumenpro((Double)feature.getAttribute("Volumenpro"));
-                poligono.setRendimient((Double)feature.getAttribute("Rendimient"));
-                poligono.setPrecio((Double)feature.getAttribute("Precio"));
-                poligono.setValorprodu((Double)feature.getAttribute("Valorprodu"));
-                poligono.setCveent((String)feature.getAttribute("CVE_ENT"));
-                poligono.setNoment(new String(((String)feature.getAttribute("NOM_ENT")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setCvemun((String)feature.getAttribute("CVE_MUN"));
-                poligono.setNomgeo(new String(((String)feature.getAttribute("NOMGEO")).getBytes(),StandardCharsets.UTF_8));
-                poligono.setCvegeo((String)feature.getAttribute("CVEGEO"));
-                poligono.setX((Double)feature.getAttribute("X"));
-                poligono.setY((Double)feature.getAttribute("Y"));
-                poligono.setShapeleng((Double)feature.getAttribute("Shape_leng"));
-                poligono.setShapearea((Double)feature.getAttribute("Shape_area"));
+                Shape shape = new Shape();
+                shape.setAnio((Long)feature.getAttribute("Anio"));
+                shape.setIdestado(Long.valueOf((String)feature.getAttribute("Idestado")));
+                shape.setNomestado(new String(((String)feature.getAttribute("Nomestado")).getBytes(),StandardCharsets.UTF_8));
+                shape.setIdddr((Long)feature.getAttribute("Idddr"));
+                shape.setNomddr(new String(((String)feature.getAttribute("Nomddr")).getBytes(),StandardCharsets.UTF_8));
+                shape.setIdcader((Long)feature.getAttribute("Idcader"));
+                shape.setNomcader(new String(((String)feature.getAttribute("Nomcader")).getBytes(),StandardCharsets.UTF_8));
+                shape.setIdmunicipio(Long.valueOf((String)feature.getAttribute("Idmunicipi")));
+                shape.setNommunicip(new String(((String)feature.getAttribute("Nommunicip")).getBytes(),StandardCharsets.UTF_8));
+                shape.setConcate(Long.valueOf((String)feature.getAttribute("Concate")));
+                shape.setIdciclo((Long)feature.getAttribute("Idciclo"));
+                shape.setNomcliclopr((String)feature.getAttribute("Nomciclopr"));
+                shape.setIdmodalida((Long)feature.getAttribute("Idmodalida"));
+                shape.setNommodalid((String)feature.getAttribute("Nommodalid"));
+                shape.setIdunidadme((Long)feature.getAttribute("Idunidadme"));
+                shape.setNomunidad((String)feature.getAttribute("Nomunidad"));
+                shape.setIdcultivo((Long)feature.getAttribute("Idcultivo"));
+                shape.setNomcultivo((String)feature.getAttribute("Nomcultivo"));
+                shape.setSembrada((Double)feature.getAttribute("Sembrada"));
+                shape.setCosechada((Double)feature.getAttribute("Cosechada"));
+                shape.setSiniestrad((Double)feature.getAttribute("Siniestrad"));
+                shape.setVolumenpro((Double)feature.getAttribute("Volumenpro"));
+                shape.setRendimient((Double)feature.getAttribute("Rendimient"));
+                shape.setPrecio((Double)feature.getAttribute("Precio"));
+                shape.setValorprodu((Double)feature.getAttribute("Valorprodu"));
                 
-                poligonoIterator.add(poligono);
+                poligonoIterator.add(shape);
                 }
             featureIterator.close();
             
             }
 		return poligonoIterator;
 		}
+	
+	public static List<Poligono> readGeometryIntoPolygon(List<String> shapesNames) throws IOException{
+		List<Poligono> poligonoIterator = new ArrayList<>();
+        
+		for (String shapeName : shapesNames) {
+			URL shapeURL = new File(shapeName).toURI().toURL();
+        	Map<String, Serializable> params = new HashMap<>();
+            params.put("url", shapeURL);
+            
+            DataStore dataStore = DataStoreFinder.getDataStore(params);
+            String typeName = dataStore.getTypeNames()[0];
+        	FeatureSource<SimpleFeatureType, SimpleFeature> source = dataStore.getFeatureSource(typeName);
+        	
+            FeatureCollection<SimpleFeatureType, SimpleFeature> featureCollection = source.getFeatures();
+            FeatureIterator<SimpleFeature> featureIterator = featureCollection.features();
+            
+            while (featureIterator.hasNext()) {
+            	SimpleFeature feature = featureIterator.next();
+                Poligono poligono = new Poligono();
+                poligono.setThe_geom((Geometry) feature.getDefaultGeometry());
+                
+                poligonoIterator.add(poligono);
+            }
+            featureIterator.close();
+		}
+        
+       return poligonoIterator;
+       }
+	
 	
 	}
